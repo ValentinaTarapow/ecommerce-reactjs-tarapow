84cde217580c8aa2ca8400206fe3a87274efc11c
ValentinaTarapow <tarapow.v@gmail.com>
2021-11-08 20:17:23 -0300

    creating NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+const NavBar = () =>{
+
+    return(
+        <div className="header">
+            <h1 className="brand">The Hammy Store</h1>
+
+            <nav>
+                <ul>
+                    <li> <a href="">Productos</a> </li>
+                    <li> <a href="">Contacto</a></li>
+                    <li> <a href="">Preguntas frecuentes</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
