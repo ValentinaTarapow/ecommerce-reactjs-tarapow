7b7fa659c7e8594268df04ab0b8ae02d50b8e787
ValentinaTarapow <tarapow.v@gmail.com>
2021-11-08 20:27:50 -0300

    NavBar final touches

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,21 @@
+import { Fragment } from "react";
+
 const NavBar = () =>{
 
     return(
-        <div className="header">
-            <h1 className="brand">The Hammy Store</h1>
+        <Fragment>
+            <header>
+                <h1 className="brand">The Hammy Store</h1>
 
-            <nav>
-                <ul>
-                    <li> <a href="">Productos</a> </li>
-                    <li> <a href="">Contacto</a></li>
-                    <li> <a href="">Preguntas frecuentes</a></li>
-                </ul>
-            </nav>
-        </div>
+                <nav>
+                    <ul>
+                        <li> <a href="">Productos</a> </li>
+                        <li> <a href="">Contacto</a></li>
+                        <li> <a href="">Preguntas frecuentes</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </Fragment>
     )
 }
 
