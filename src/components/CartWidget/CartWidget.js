045db905e211bb8173686af1d6071f1e93984910
ValentinaTarapow <tarapow.v@gmail.com>
2021-12-06 20:35:14 -0300

    Class 11 - Rendering techniques

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -5,10 +5,10 @@ import './CartWidget.scss';
 
 const CartWidget = () =>{
 
-    const {totalAmount} = useContext(CartContext);
+    const {cart,totalAmount} = useContext(CartContext);
     return(
         <Fragment>
-            <btn className="btn-shopping-cart">
+            <btn className={cart.length === 0 ? 'hidden widget btn-shopping-cart' : 'btn-shopping-cart' }>
                 <HiShoppingCart size="24" className="ms-2"/>
                 <span className className="mx-2">{totalAmount()}</span>
             </btn>
