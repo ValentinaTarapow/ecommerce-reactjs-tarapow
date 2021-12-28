6b5141ebdbd4499e7513c43e244de6fb71d1a68a
ValentinaTarapow <tarapow.v@gmail.com>
2021-12-28 19:40:28 -0300

    Custom hook GenerateOrder

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,12 +1,10 @@
 import React, { useContext, useState } from 'react'
 import { Navigate } from 'react-router'
 import { CartContext } from '../../context/CartContext'
-import { db } from '../../firebase/config'
 import { validateData } from '../../helpers/validateData'
-import { collection, Timestamp, writeBatch, query, where, documentId, getDocs, addDoc } from 'firebase/firestore'
-import Swal from 'sweetalert2';
 import "./Checkout.scss";
 import {AiOutlineSend} from "react-icons/ai";
+import { generateOrder } from '../../firebase/generateOrder'
 
 export const Checkout = () => {
 
@@ -26,59 +24,12 @@ export const Checkout = () => {
         })
     }
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
         if (!validateData(values)) { return }
 
-        const order = {
-            buyer: {...values},
-            items: cart,
-            total: totalPrice(),
-            date: Timestamp.fromDate( new Date() )
-        }
-
-        const batch = writeBatch(db)
-
-        const ordersRef = collection(db, "orders")
-        const productsRef = collection(db, "products")
-        const q = query(productsRef, where(documentId(), 'in', cart.map(el => el.id)))
-
-        const outOfStock = []
-
-        const products = await getDocs(q)
-
-        products.docs.forEach((doc) => {
-            const itemToUpdate = cart.find((prod) => prod.id === doc.id)
-
-            if (doc.data().stock >= itemToUpdate.counter) {
-                batch.update(doc.ref, {
-                    stock: doc.data().stock - itemToUpdate.cantidad
-                })
-            } else {
-                outOfStock.push(itemToUpdate)
-            }
-        })
-
-        if (outOfStock.length === 0) {    
-            addDoc(ordersRef, order)
-                .then((res) => {
-                    batch.commit()   
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Your order has been placed successfully',
-                        text: `Your order ID is: #${res.id}`,
-                        footer: 'We will contact you very soon to update your order status. Thank you for your purchase!'                       
-                    })
-                    clear()
-                })
-        } else {
-            Swal.fire({
-                icon: 'error',
-                title: 'There is no stock available of these products:',
-                text: outOfStock.map(el => el.name).join(', ')
-            })
-        }
-}
+        generateOrder(values, cart, totalPrice, clear);
+    }
 
 
     return (
---
+++ src/firebase/generateOrder.js
@@ -0,0 +1,58 @@
+import {writeBatch, collection, query, where, documentId, getDocs, addDoc, Timestamp} from 'firebase/firestore/lite';
+import Swal from 'sweetalert2';
+import {db} from './config';
+
+export const generateOrder =  async (values, cart, totalPrice, clear) => {
+
+    const order = {
+        buyer: {...values},
+        items: cart,
+        total: totalPrice(),
+        date: Timestamp.fromDate( new Date() )
+    }
+
+    const batch = writeBatch(db)
+
+    const ordersRef = collection(db,"orders")
+    const productsRef = collection(db, "products")
+    const q = query(productsRef, where(documentId(), 'in', cart.map(el => el.id)))
+
+    const outOfStock = []
+
+    const products = await getDocs(q)
+
+    products.docs.forEach((doc) =>{
+
+        const itemToUpdate = cart.find((prod) => prod.id === doc.id);
+        
+        if(doc.data().stock >= itemToUpdate.counter){
+            batch.update(doc.ref, {
+                stock: doc.data().stock - itemToUpdate.counter
+            })
+        } else{
+            outOfStock.push(itemToUpdate);
+        }
+
+    })
+
+    if(outOfStock.length === 0){
+        addDoc(ordersRef, order)
+            .then((res) => {
+                batch.commit()   
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Your order has been placed successfully',
+                        text: `Your order ID is: #${res.id}`,
+                        footer: 'We will contact you very soon to update your order status. Thank you for your purchase!'                       
+                    })
+                    clear();
+            })
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'There is no stock available of these products:',
+            text: outOfStock.map(el => el.name).join(', ')
+        })
+    }
+    
+}
