09e8bb57ca006cf2f43983d9de4281a24df354b4
ValentinaTarapow <tarapow.v@gmail.com>
2021-12-28 14:26:24 -0300

    Improve footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,34 @@
-import { Fragment } from "react";
-import "./Footer.scss"; 
-import { Container } from 'react-bootstrap'
+import "./Footer.scss";
+import React from "react"; 
 import {AiOutlinePhone} from 'react-icons/ai'
 import {AiOutlineMail} from 'react-icons/ai'
+import { AiOutlineGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { FaRegHeart } from "react-icons/fa";
+
 
 export const Footer = () =>{
     return(
-    <footer className="footer w-100">
-        <div>
-            <h4>Contacto</h4>
-            <h5></h5>
-
+    <footer className="footer w-100 d-flex flex-row justify-content-between">
+        <div className="text-center">
+            <h4 className="mb-4">Contact Us</h4>
             <p><AiOutlinePhone /> +54 9 1169972991</p>
             <p><AiOutlineMail /> valentinadct@gmail.com</p>
+        </div>
+
+        <div className="text-center">
+            <h4 className="mb-4">Escentially</h4>
+            <p>Perfuming your dreams since 2021</p>
+        </div>
+
+        <div className="text-center">
+            <h4 className="mb-4">Credits</h4>
+            <p>Made with < FaRegHeart className="heart"/> by Valentina Tarapow</p>
+            <div className="d-flex justify-content-center">
+                <a href="https://github.com/ValentinaTarapow" className="me-2"><AiOutlineGithub  size={24}/></a>
+                <a href="https://www.linkedin.com/in/valentina-tarapow/"><AiFillLinkedin  size={24}/></a>
+            </div>
+
         </div>
     </footer>
 
