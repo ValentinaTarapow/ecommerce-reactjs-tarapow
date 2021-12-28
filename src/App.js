b3116429a4785ffc9b46c96c9767b12a02283f30
ValentinaTarapow <tarapow.v@gmail.com>
2021-12-28 14:25:37 -0300

    Add Banner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.scss';
 import NavBar from './components/NavBar/NavBar';
-import React, { useState } from 'react';
+import {Banner} from './components/Banner/Banner'
+import React from 'react';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import {Footer} from './components/Footer/Footer';
 import {ItemDetailContainer} from './components/ItemDetailContainer/ItemDetailContainer';
@@ -9,12 +10,15 @@ import {Cart} from './components/Cart/Cart';
 import {BrowserRouter, Route , Routes, Navigate} from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import { Checkout } from './components/Checkout/Checkout';
+import { Page404 } from './components/DeadEnds/Page404/Page404';
 
 function App() {
 
   return (
     <CartProvider>
+      <Banner/>
       <BrowserRouter>
+          
           <NavBar />
         
           <Routes>
@@ -23,7 +27,8 @@ function App() {
             <Route path="item/:itemId" element={ <ItemDetailContainer /> } />
             <Route path="cart" element={ <Cart /> } />
             <Route path="checkout" element={ <Checkout /> } />
-            <Route path="*" element={ < Navigate to="/" /> } />
+            {/* <Route path="*" element={ < Navigate to="/" /> } /> */}
+            <Route path="*" element={ < Page404 /> } />
           </Routes>
 
         
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import { MdLocalShipping } from "react-icons/md";
+
+import React from "react";
+import { Button } from 'react-bootstrap';
+import './Banner.scss'; 
+
+export const Banner = () => {
+
+    return(
+        <div className="banner">
+            <p>free shipping to the whole country <MdLocalShipping /></p>
+        </div>
+    )
+}
